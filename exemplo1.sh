541c9bd2c36dcba8085984313668dc32ba94c1a2
AtilaMonteblanco <atilaunno@gmail.com>
2016-06-21 22:27:55 -0300

    envio os exercicis do script

---
+++ exemplo1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Seu nome de usuário é:"
+ÁtilaMonteblanco
+echo "Info de hora atual e tempo que o computador esta ligado"
+uptime
+echo "O script esta executando do diretório:"
+pwd
+
+
